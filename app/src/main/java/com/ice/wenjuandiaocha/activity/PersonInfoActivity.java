aefe3c6356c8575e56267a3fbfe3b3526fe9052d
ice <lucifervillage@gmail.com>
2016-03-08 15:54:38 +0800

    make personInfo table

--- app/src/main/java/com/ice/wenjuandiaocha/activity/PersonInfoActivity.java
+++ app/src/main/java/com/ice/wenjuandiaocha/activity/PersonInfoActivity.java
@@ -2,6 +2,8 @@ package com.ice.wenjuandiaocha.activity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,6 +16,10 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.ice.greendao.DaoMaster;
+import com.ice.greendao.DaoSession;
+import com.ice.greendao.PersonInfo;
+import com.ice.greendao.PersonInfoDao;
 import com.ice.wenjuandiaocha.R;
 import com.ice.wenjuandiaocha.util.DatePickerFragment;
 
@@ -24,6 +30,7 @@ import butterknife.ButterKnife;
 
 public class PersonInfoActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
+    private static final String TAG = "PersonInfoActivity";
     @Bind(R.id.name)
     EditText name;
     @Bind(R.id.sex)
@@ -68,11 +75,24 @@ public class PersonInfoActivity extends AppCompatActivity implements DatePickerD
     Button add;
     private DatePickerFragment newFragment;
 
+    private SQLiteDatabase db;
+    private DaoMaster daoMaster;
+    private DaoSession daoSession;
+    private Cursor cursor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_info);
         ButterKnife.bind(this);
+
+        // 官方推荐将获取 DaoMaster 对象的方法放到 Application 层，这样将避免多次创建生成 Session 对象
+        setupDatabase();
+        // 获取 NoteDao 对象
+        getPersonInfoDao();
+
+        cursor = db.query(getPersonInfoDao().getTablename(), getPersonInfoDao().getAllColumns(), null, null, null, null, null);
+
 
         ArrayAdapter<CharSequence> sexAdapter = ArrayAdapter.createFromResource(this,
                 R.array.sex_array, android.R.layout.simple_spinner_item);
@@ -114,15 +134,15 @@ public class PersonInfoActivity extends AppCompatActivity implements DatePickerD
         oldhelpAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         oldhelp.setAdapter(oldhelpAdapter);
 
-
-        sex.setOnItemSelectedListener(listener);
-        education.setOnItemSelectedListener(listener);
-        occupation.setOnItemSelectedListener(listener);
-        marriage.setOnItemSelectedListener(listener);
-        payment.setOnItemSelectedListener(listener);
-        home.setOnItemSelectedListener(listener);
-        economy.setOnItemSelectedListener(listener);
-        oldhelp.setOnItemSelectedListener(listener);
+//
+//        sex.setOnItemSelectedListener(listener);
+//        education.setOnItemSelectedListener(listener);
+//        occupation.setOnItemSelectedListener(listener);
+//        marriage.setOnItemSelectedListener(listener);
+//        payment.setOnItemSelectedListener(listener);
+//        home.setOnItemSelectedListener(listener);
+//        economy.setOnItemSelectedListener(listener);
+//        oldhelp.setOnItemSelectedListener(listener);
 
         birthday.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -134,6 +154,29 @@ public class PersonInfoActivity extends AppCompatActivity implements DatePickerD
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // 插入操作，简单到只要你创建一个 Java 对象
+                PersonInfo note = new PersonInfo(null, name.getText().toString(), sex.getSelectedItem().toString(), birthday.getText().toString()
+                        , height.getText().toString()
+                        , weight.getText().toString()
+                        , provider.getText().toString()
+                        , relation.getText().toString()
+                        , religion.getText().toString()
+                        , idcard.getText().toString()
+                        , education.getSelectedItem().toString()
+                        , occupation.getSelectedItem().toString()
+                        , marriage.getSelectedItem().toString()
+                        , payment.getSelectedItem().toString()
+                        , home.getSelectedItem().toString()
+                        , economy.getSelectedItem().toString()
+                        , oldhelp.getSelectedItem().toString()
+                        , accident.getText().toString()
+                        , wish1.getText().toString()
+                        , wish2.getText().toString());
+                getPersonInfoDao().insert(note);
+                Log.d(TAG, "Inserted new note, ID: " + note.getId());
+                cursor.requery();
+
                 Intent intent = new Intent(PersonInfoActivity.this, SimpleInfoActivity.class);
                 startActivity(intent);
             }
@@ -142,38 +185,54 @@ public class PersonInfoActivity extends AppCompatActivity implements DatePickerD
 
     }
 
+    private void setupDatabase() {
+        // 通过 DaoMaster 的内部类 DevOpenHelper，你可以得到一个便利的 SQLiteOpenHelper 对象。
+        // 可能你已经注意到了，你并不需要去编写「CREATE TABLE」这样的 SQL 语句，因为 greenDAO 已经帮你做了。
+        // 注意：默认的 DaoMaster.DevOpenHelper 会在数据库升级时，删除所有的表，意味着这将导致数据的丢失。
+        // 所以，在正式的项目中，你还应该做一层封装，来实现数据库的安全升级。
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "notes-db", null);
+        db = helper.getWritableDatabase();
+        // 注意：该数据库连接属于 DaoMaster，所以多个 Session 指的是相同的数据库连接。
+        daoMaster = new DaoMaster(db);
+        daoSession = daoMaster.newSession();
+    }
 
-    AdapterView.OnItemSelectedListener listener = new AdapterView.OnItemSelectedListener() {
-        @Override
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            switch (parent.getId()) {
-                case R.id.sex:
-                    Log.e("dddd", "ddddddddddddddddddddddddddddddddddddddddddddd" + position);
-                    break;
-                case R.id.education:
-                    Log.e("dddd", "ddddddddddddddddddddddddddddddddddddddddddddd" + position);
-                    break;
-                case R.id.occupation:
-                    Log.e("dddd", "ddddddddddddddddddddddddddddddddddddddddddddd" + position);
-                    break;
-                case R.id.marriage:
-                    break;
-                case R.id.payment:
-                    break;
-                case R.id.home:
-                    break;
-                case R.id.economy:
-                    break;
-                case R.id.oldhelp:
-                    break;
-            }
-        }
+    private PersonInfoDao getPersonInfoDao() {
+        return daoSession.getPersonInfoDao();
+    }
 
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-
-        }
-    };
+//
+//    AdapterView.OnItemSelectedListener listener = new AdapterView.OnItemSelectedListener() {
+//        @Override
+//        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//            switch (parent.getId()) {
+//                case R.id.sex:
+//                    Log.e("dddd", "ddddddddddddddddddddddddddddddddddddddddddddd" + position);
+//                    break;
+//                case R.id.education:
+//                    Log.e("dddd", "ddddddddddddddddddddddddddddddddddddddddddddd" + position);
+//                    break;
+//                case R.id.occupation:
+//                    Log.e("dddd", "ddddddddddddddddddddddddddddddddddddddddddddd" + position);
+//                    break;
+//                case R.id.marriage:
+//                    break;
+//                case R.id.payment:
+//                    break;
+//                case R.id.home:
+//                    break;
+//                case R.id.economy:
+//                    break;
+//                case R.id.oldhelp:
+//                    break;
+//            }
+//        }
+//
+//        @Override
+//        public void onNothingSelected(AdapterView<?> parent) {
+//
+//        }
+//    };
 
     public void showDatePickerDialog(View v) {
         newFragment = new DatePickerFragment();
--- mygreendaolib/src/main/java/MyDaoGenerator.java
+++ mygreendaolib/src/main/java/MyDaoGenerator.java
@@ -1,8 +1,7 @@
-package com.example;
-
 import de.greenrobot.daogenerator.DaoGenerator;
 import de.greenrobot.daogenerator.Entity;
 import de.greenrobot.daogenerator.Schema;
+import javafx.scene.control.Spinner;
 
 
 public class MyDaoGenerator {
@@ -23,7 +22,7 @@ public class MyDaoGenerator {
 
         // 最后我们将使用 DAOGenerator 类的 generateAll() 方法自动生成代码，此处你需要根据自己的情况更改输出目录（既之前创建的 java-gen)。
         // 其实，输出目录的路径可以在 build.gradle 中设置，有兴趣的朋友可以自行搜索，这里就不再详解。
-        new DaoGenerator().generateAll(schema, "../app/src/main/java-gen");
+        new DaoGenerator().generateAll(schema, "E:/Android/AndroidStudioWorkplace/WenJuanDiaoCha/app/src/main/java-gen");
     }
 
     /**
@@ -31,17 +30,38 @@ public class MyDaoGenerator {
      */
     private static void addNote(Schema schema) {
         // 一个实体（类）就关联到数据库中的一张表，此处表名为「Note」（既类名）
-        Entity note = schema.addEntity("Note");
+        Entity note = schema.addEntity("PersonInfo");
         // 你也可以重新给表命名
         // note.setTableName("NODE");
 
         // greenDAO 会自动根据实体类的属性值来创建表字段，并赋予默认值
         // 接下来你便可以设置表中的字段：
         note.addIdProperty();
-        note.addStringProperty("text").notNull();
+        note.addStringProperty("name").notNull();
         // 与在 Java 中使用驼峰命名法不同，默认数据库中的命名是使用大写和下划线来分割单词的。
         // For example, a property called “creationDate” will become a database column “CREATION_DATE”.
-        note.addStringProperty("comment");
-        note.addDateProperty("date");
+        note.addStringProperty("sex");
+        note.addStringProperty("birthday");
+
+
+        note.addStringProperty("height");
+        note.addStringProperty("weight");
+        note.addStringProperty("provider");
+        note.addStringProperty("relation");
+        note.addStringProperty("religion");
+        note.addStringProperty("idcard");
+        note.addStringProperty("education");
+        note.addStringProperty("occupation");
+        note.addStringProperty("marriage");
+        note.addStringProperty("payment");
+        note.addStringProperty("home");
+        note.addStringProperty("economy");
+        note.addStringProperty("oldhelp");
+        note.addStringProperty("accident");
+        note.addStringProperty("wish1");
+        note.addStringProperty("wish2");
+
+
+
     }
 }
